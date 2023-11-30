a9602dba0ec4a09e6e6be04dd0092d0823194995
unknown <rizalyuniar123@gmail.com>
2023-11-30 11:44:58 +0700

    role di controller

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,16 +1,12 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException, SetMetadata } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core'; // Perhatikan perubahan di sini
 
 @Injectable()
-export class AuthGuardAdmin implements CanActivate {
-  constructor(private jwtService: JwtService) { }
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -26,7 +22,8 @@ export class AuthGuardAdmin implements CanActivate {
         { secret: jwtConstants.secret }
       );
       request['admin'] = payload;
-      const allowedRole = ['admin'];
+
+      const allowedRole = this.getAllowedRoles(context);
 
       if (!allowedRole.includes(payload.role)) {
         throw new UnauthorizedException('Akses tidak diizinkan. Kamu bukan admin.');
@@ -45,43 +42,11 @@ export class AuthGuardAdmin implements CanActivate {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
-}
 
-@Injectable()
-export class AuthGuardKasir implements CanActivate {
-  constructor(private jwtService: JwtService) { }
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-
-    if (!token) {
-      throw new UnauthorizedException('Akses tidak diizinkan. Token tidak ditemukan.');
-    }
-
-    try {
-      const payload = await this.jwtService.verifyAsync(
-        token,
-        { secret: jwtConstants.secret }
-      );
-      request['admin'] = payload;
-      const allowedRole = ['admin'];
-
-      if (!allowedRole.includes(payload.role)) {
-        throw new UnauthorizedException('Akses tidak diizinkan. Kamu bukan Kasir.');
-      }
-    } catch (error) {
-      console.error(error);
-      if (error instanceof UnauthorizedException && error.message === 'Akses tidak diizinkan. Kamu bukan Kasir.') {
-        throw error;
-      }
-      throw new UnauthorizedException('Token tidak valid.');
-    }
-    return true;
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
+  private getAllowedRoles(context: ExecutionContext): string[] {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler()) || [];
+    return Array.isArray(roles) ? roles : [roles];
   }
 }
+
+export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
--- src/tenant/tenant.controller.ts
+++ src/tenant/tenant.controller.ts
@@ -2,14 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } fro
 import { TenantService } from './tenant.service';
 import { CreateTenantDto } from './dto/create-tenant.dto';
 import { UpdateTenantDto } from './dto/update-tenant.dto';
-import { AuthGuardAdmin, AuthGuardKasir } from 'src/auth/auth.guard';
+import { AuthGuard, Roles } from 'src/auth/auth.guard';
 import { InternalServerErrorException } from '@nestjs/common';
 
 @Controller('tenant')
 export class TenantController {
   constructor(private readonly tenantService: TenantService) {}
   @Post()
-  @UseGuards(AuthGuardAdmin)
+  @UseGuards(AuthGuard)
+  @Roles('admin')
   async create(@Body() createTenantDto: CreateTenantDto) {
     try {
       return await this.tenantService.create(createTenantDto);
@@ -20,21 +21,18 @@ export class TenantController {
   }
 
   @Get()
-  @UseGuards(AuthGuardKasir)
+  @UseGuards(AuthGuard)
   findAll() {
     return this.tenantService.findAll();
   }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tenantService.findOne(id);
   }
-
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updateTenantDto: UpdateTenantDto) {
   //   return this.tenantService.update(+id, updateTenantDto);
   // }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tenantService.remove(id);
